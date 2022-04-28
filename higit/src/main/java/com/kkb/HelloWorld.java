4aee6fb46a357fb04e70b538c7d2885cff07072e
guoaj <guoaijun008@163.com>
2022-04-28 12:52:44 +0800

    Update HelloWorld.java
    增加了两行代码

--- higit/src/main/java/com/kkb/HelloWorld.java
+++ higit/src/main/java/com/kkb/HelloWorld.java
@@ -3,5 +3,7 @@ package com.kkb;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2222");
+        System.out.println("hello world2333");
     }
 }
