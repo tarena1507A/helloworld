bb5a164f68e80defeb9bb58d5c1b855325075ed5
guoaj <guoaijun008@163.com>
2022-04-29 14:50:31 +0000

    update higit/src/main/java/com/kkb/HelloWorld.java.  远程修改代码 增加了一行代码  远程添加

--- higit/src/main/java/com/kkb/HelloWorld.java
+++ higit/src/main/java/com/kkb/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("本地增加代码");
         System.out.println("hello world2222");
         System.out.println("hello world2333");
+        System.out.println("远程添加代码");
     }
 }
